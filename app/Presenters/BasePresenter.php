a88d891c794eb4fb5da1960e448a1f8ac5413f73
Jakub Záruba <info@jakub-zaruba.cz>
2020-10-10 17:54:44 +0200

    Style improvement (menu header)

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use Nette\Neon\Neon;
 use Nette\Application\UI\Presenter;
+use Nette\Localization\ITranslator;
 use App\Controls\MenuControl\MenuControl;
 use App\Controls\MenuControl\MenuControlFactory;
+use Contributte\Translation\LocalesResolvers\Session;
 
 abstract class BasePresenter extends Presenter
 {
@@ -14,10 +17,28 @@ abstract class BasePresenter extends Presenter
     /** @var MenuControlFactory @inject */
     public $menuControlFactory;
 
+    /** @var ITranslator @inject */
+    public $translator;
+
+    /** @var Session @inject */
+    public $translatorSessionResolver;
+
+    public function beforeRender()
+    {
+        parent::beforeRender();
+        $this->template->styleLastMod = filemtime(__DIR__ . '/../../www/css/styles.css');
+    }
+
+    public function handleChangeLocale(string $locale): void
+    {
+        $this->translatorSessionResolver->setLocale($locale);
+        $this->redirect('this');
+    }
+
     protected function createComponentMenu(): MenuControl
     {
         $homePage = $this->getPresenter()->getName() == "Homepage";
-        
+
         return $this->menuControlFactory->create($homePage);
     }
 }
--- app/Presenters/ServicesPresenter.php
+++ app/Presenters/ServicesPresenter.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use Nette\Neon\Neon;
+
 final class ServicesPresenter extends BasePresenter
 {
 }
